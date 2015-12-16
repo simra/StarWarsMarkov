593e6be1d89aa5d611b001d86b737b0a2c355fa0
simra <robert.sim@live.com>
2015-12-15 21:49:14 -0800

    Enable random seeding. Add the ability to pass a corpus on the command line.

--- Program.fs
+++ Program.fs
@@ -1,7 +1,6 @@
-﻿// Learn more about F# at http://fsharp.org
-// See the 'F# Tutorial' project for more help.
-open ParseScripts
+﻿open ParseScripts
 open System
+open System.IO
 open System.Text.RegularExpressions
 
 let train str =
@@ -14,8 +13,15 @@ let train str =
     |> Seq.groupBy (fun ((a,b),c) -> a)
     |> Map.ofSeq
 
+
 let rnd=new Random()
-    
+
+let randomSeed m =
+    Map.toSeq m
+    |> Seq.map (fun (s,_)->s)
+    |> Array.ofSeq
+    |> fun a -> a.[rnd.Next(a.Length)]
+
 let query (m:Map<string,((string*string)*int) seq>) (qq:string) =
     let choose (s:((string*string)*int) seq) : (string*string)*int =
         let l=rnd.Next(Seq.length s)
@@ -23,21 +29,42 @@ let query (m:Map<string,((string*string)*int) seq>) (qq:string) =
         |> Seq.take 1
         |> Seq.exactlyOne
 
-    
+    let qq'= 
+        if qq="rand" then
+            randomSeed m
+        else qq
+
     Seq.unfold (fun (q,i) -> 
         if i=0 then None
         else if Map.containsKey q m then 
             let ((a,b),c)=choose m.[q]
             Some(b,(b,i-1))
         else None // What to do?
-        ) (qq,100)
+        ) (qq',100)
+        |> Seq.map (fun s -> if s.Contains(" ") then s.Split(' ').[1] else s)
+        |> String.concat " "
+        |> fun s -> qq'+" "+s
+            
+// seed the training with additional corpi: pass them as filenames on the command line (text only)  
+// Pass them multiple times to give them more weight (TODO: make this configurable)
+let ParseArgs (argv:string[]) =
+    [0..argv.Length-1]
+    |> Seq.map (fun x -> 
+            eprintfn "Reading %s" argv.[x]
+            File.ReadAllLines(argv.[x]))
+    |> Seq.map (fun lines -> lines|>Seq.takeWhile (fun s -> not (s.StartsWith("*** END OF THIS PROJECT GUTENBERG EBOOK"))))
+    |> Seq.concat
+    |> String.concat "\n"
 
 [<EntryPoint>]
 let main argv = 
     let m = 
-        ParseScripts.scriptUrls
-        |> List.map (fun (t,u) -> (ParseScripts.getScript u))    
-        |> Seq.map (fun x -> string x)
+        [ ParseScripts.scriptUrls
+            |> List.map (fun (t,u) -> (ParseScripts.getScript u))    
+            |> Seq.map (fun x -> string x)
+            |> Seq.map (fun x -> x.Replace("<b>","").Replace("</b>",""))
+            |> String.concat "\n";
+          ParseArgs argv]
         |> String.concat "\n"
         |> train
 
@@ -47,16 +74,13 @@ let main argv =
     let lookup = "LEIA </b>"
     
     while (true) do
-        printfn "Enter a two-token seed. eg \"Help me\""
+        printfn "Enter a two-token seed: eg \"the Force\", or \"rand\" to pick a random seed."
         let l=Console.ReadLine()
-        if l.Split(' ').Length<>2 then 
+        if l<>"rand" && l.Split(' ').Length<>2 then 
             eprintfn "Query must be two tokens in length"
         else             
             //query m lookup
             query m l
            // |> Seq.map (fun ((a,b),c) -> a)
-            |> Seq.map (fun s -> if s.Contains(" ") then s.Split(' ').[1] else s)
-            |> String.concat " "
-            |> fun s -> l + " "+s
             |> printfn "%s"
     0 // return an integer exit code
